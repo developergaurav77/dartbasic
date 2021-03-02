9358c20b81bb90c499afd32699f0bfb99acb7d3a
developergaurav77 <developer.gaurav77@gmail.com>
2021-03-02 09:37:05 +0545

    function returning

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,15 @@ void main() {
   var surname = "Thagunna";
   var age = 22;
   var height = 5.8;
-  fun(surname,age,height);
-  fun('Bhandari',23,5.6);
+  final person1 = fun(surname,age,height);
+  final person2 = fun('Bhandari',23,5.6);
+  print(person1);
+  print(person2);
 
  
 }
 
-void fun(String surname,int age, double height){
-  print("gaurav $surname");
+String fun(String sur,int age, double height){
+  return "gaurav $sur. my height is $height feet, my age is $age years old.";
 
-  print("my height is $height feet.");
-  print("my age is $age years old.");
 }
