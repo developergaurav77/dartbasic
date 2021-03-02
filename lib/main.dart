ce7eb7462dbe73d419d28a7e8bebffa8c29704cb
developergaurav77 <developer.gaurav77@gmail.com>
2021-03-02 09:29:06 +0545

    function adding

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ void main() {
   var surname = "Thagunna";
   var age = 22;
   var height = 5.8;
+  fun(surname,age,height);
+  fun('Bhandari',23,5.6);
+
+ 
+}
+
+void fun(String surname,int age, double height){
   print("gaurav $surname");
-  print("My surname has ${surname.length} letters");
+
   print("my height is $height feet.");
   print("my age is $age years old.");
 }
