76cc9b9adc61ea4e8a56ebbbbb35a1c4d41d5e2c
developergaurav77 <developer.gaurav77@gmail.com>
2021-03-02 09:51:23 +0545

    Uisng named parameters

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,15 @@ void main() {
   var surname = "Thagunna";
   var age = 22;
   var height = 5.8;
-  final person1 = fun(surname,age);
-  final person2 = fun('Bhandari',23,5.6);
+  final person1 = fun(sur:surname,age:age,height: height);
+  final person2 = fun(sur:'Bhandari',age:23,height:5.6);
   print(person1);
   print(person2);
 
  
 }
 
-String fun(String sur,int age, [double height = 4.0]){
+String fun({String sur,int age, double height = 4.0}){
   return "gaurav $sur. my height is $height feet, my age is $age years old.";
 
 }
