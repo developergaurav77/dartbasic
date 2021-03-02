2a7922ee08fa6e6c6a69ba7ac203eacbb69c2411
developergaurav77 <developer.gaurav77@gmail.com>
2021-03-02 09:09:32 +0545

    datatypes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 void main() {
   String surname = "Thagunna";
+  int age = 22;
+  double height = 5.8;
   print("gaurav $surname");
   print("My surname has ${surname.length} letters");
+  print("my height is $height feet.");
+  print("my age is $age years old.");
 }
