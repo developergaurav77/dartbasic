40ae7f0e25e2571c67a9034b954dc6eebe745add
developergaurav77 <developer.gaurav77@gmail.com>
2021-03-02 09:44:12 +0545

    making height parameter optional and default

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ void main() {
   var surname = "Thagunna";
   var age = 22;
   var height = 5.8;
-  final person1 = fun(surname,age,height);
+  final person1 = fun(surname,age);
   final person2 = fun('Bhandari',23,5.6);
   print(person1);
   print(person2);
@@ -10,7 +10,7 @@ void main() {
  
 }
 
-String fun(String sur,int age, double height){
+String fun(String sur,int age, [double height = 4.0]){
   return "gaurav $sur. my height is $height feet, my age is $age years old.";
 
 }
