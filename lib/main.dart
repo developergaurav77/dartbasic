82d0f1e18d56c732a41e3ac55579e965d2567c25
developergaurav77 <developer.gaurav77@gmail.com>
2021-03-04 10:34:39 +0545

    for loop completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 void main(){
   final values = [1,2,3,4];
+  print(" the sum of $values is ${sum(values)}");
+}
+int sum(List<int> values){
+  int i =0;
+  int result= 0;
+  for(i=0;i<values.length;i++){
+    result = result + values[i];
+  }
+  return result;
+
+}
+
+
+
+
+
+
+
+
+
+/*void main(){
+  final values = [1,2,3,4];
   print(sum(values));
 }
 int sum(List<int> values){
@@ -11,3 +33,4 @@ int sum(List<int> values){
   }
   return result;
 }
+*/
