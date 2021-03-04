18920762a258f6f0b1b55e01d40fda931d0be1d4
developergaurav77 <developer.gaurav77@gmail.com>
2021-03-04 09:28:32 +0545

    map done

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
-void main() {
-  var oddnum = [1,3,5,7];
-  print(oddnum);
-
-  oddnum.add(9);
-  print(oddnum);
-  
-  oddnum.addAll([11,13,15]);
-  print(oddnum);
-  var len = oddnum.length;
-  print("length of list is $len");
-
-  var num = List<int>();
-  num.addAll([1,2,3,4]);
-  print(num);
-
+void main(){
+  var map = {
+    'name':'gaurav',
+    'age' : 22,
+    'height':5.6,
+    'surname': 'thagunna'
+  };
+  print(map['name']);
+  print("my name is ${map['name']} ${map['surname']}");
+  print("my height is ${map['height']}");
 }
