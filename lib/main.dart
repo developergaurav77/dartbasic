ae10cc43a6d8a83d172ab18f3105203446339aaa
developergaurav77 <developer.gaurav77@gmail.com>
2021-03-04 09:15:24 +0545

    list of generric type int added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 void main() {
   var oddnum = [1,3,5,7];
   print(oddnum);
+
   oddnum.add(9);
   print(oddnum);
+  
   oddnum.addAll([11,13,15]);
   print(oddnum);
   var len = oddnum.length;
   print("length of list is $len");
+
+  var num = List<int>();
+  num.addAll([1,2,3,4]);
+  print(num);
+
 }
